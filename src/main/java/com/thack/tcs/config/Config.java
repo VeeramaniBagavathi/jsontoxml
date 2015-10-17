214bd2a516c8e860ba8d139c76d4b74a97d0c74e
Malaiappan <malaiappan.kannan@gmail.com>
2015-10-17 23:05:46 +0200

    Integrated hotel beds using java sdk

--- src/main/java/com/thack/tcs/config/Config.java
+++ src/main/java/com/thack/tcs/config/Config.java
@@ -1,13 +1,21 @@
 package com.thack.tcs.config;  
   
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.SerializationFeature;
   
 @Configuration
 @ComponentScan("com.thack.tcs")
@@ -26,5 +34,21 @@ public class Config extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/*");
+    }
+
+    /**
+     * Configures the Json serializer for the REST API layer.
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.getObjectMapper().setSerializationInclusion(Include.NON_NULL);
+        converter.getObjectMapper().configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        converter.getObjectMapper().configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+        ByteArrayHttpMessageConverter byteArrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+        converters.add(byteArrayHttpMessageConverter);
+        converters.add(converter);
     }
 }  
